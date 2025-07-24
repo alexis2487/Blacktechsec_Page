dc2e147121c2658a7acb510f51e1fe47f8acd8ca
Alexis Martinez <110997738+alexis2487@users.noreply.github.com>
2025-07-24 10:07:04 -0500

    Delete js directory

--- js/cursor.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('mousemove', e => {
-    const sparkle = document.createElement('div');
-    sparkle.className = 'sparkle';
-    sparkle.style.left = `${e.pageX}px`;
-    sparkle.style.top = `${e.pageY}px`;
-    document.body.appendChild(sparkle);
-    setTimeout(() => sparkle.remove(), 500);
-});
